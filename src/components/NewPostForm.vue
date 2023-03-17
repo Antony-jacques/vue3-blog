76c29067760edb019b1fd442cc06e0d47c8b49a8
Antony <antony.jacques@laposte.net>
2023-03-17 08:59:03 +0100

    event from child to parent

---
+++ src/components/NewPostForm.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="form-container">
+        <h2>Ajouter un post</h2>
+        <form @submit.prevent="submitForm">
+            <div>
+                <label for="text">text</label>
+            </div>
+            <textarea v-model="content" name="text" id="text" cols="80" rows="10"></textarea>
+            <p>{{content}}</p>
+            <button type=submit>Enregistrer</button>
+        
+        </form>
+    </div>
+  
+</template>
+
+<script setup>
+    import { ref, defineEmits } from 'vue'
+
+    const emit = defineEmits(['submitedForm'])
+
+    let content = ref('')
+    function submitForm(){
+        console.log('submit')
+        emit('submitedForm', content.value)
+    }
+
+
+</script>
+
+
+
+<style>
+
+</style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="main">
+    <div class="header">
+      <h2>Les derniers possts</h2>
+      <p>counter: {{counter}}</p>
+      <button @click="increment">increase</button>
+      <NewPostForm @submitedForm="addPost" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+    import NewPostForm from '../components/NewPostForm'
+
+
+
+    const counter = ref(0);
+
+    function increment() {
+        counter.value += 1;
+    }
+
+    function addPost(payload){
+        console.log('event in parent', payload)
+    }
+</script>
+
+<style>
+</style>
