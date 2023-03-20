e1214f20888c997af6ad7d46d177054fdd9daa8c
Antony <antony.jacques@laposte.net>
2023-03-20 08:38:17 +0100

    display list of posts

--- src/components/NewPostForm.vue
+++ src/components/NewPostForm.vue
@@ -6,7 +6,11 @@
                 <label for="text">text</label>
             </div>
             <textarea v-model="content" name="text" id="text" cols="80" rows="10"></textarea>
-            <p>{{content}}</p>
+            <div>
+                <label for="image">image url</label>
+            </div>
+            <input v-model="imageUrl" type="text">
+            <p>{{imageUrl}}</p>
             <button type=submit>Enregistrer</button>
         
         </form>
@@ -20,9 +24,14 @@
     const emit = defineEmits(['submitedForm'])
 
     let content = ref('')
+    let imageUrl = ref('')
     function submitForm(){
         console.log('submit')
-        emit('submitedForm', content.value)
+        emit('submitedForm', {
+            content: content.value,
+            image: imageUrl.value
+            })
+        content.value = ''
     }
 
 
--- src/views/PostsView.vue
+++ src/views/PostsView.vue
@@ -2,8 +2,10 @@
   <div class="main">
     <div class="header">
       <h2>Les derniers possts</h2>
-      <p>counter: {{counter}}</p>
-      <button @click="increment">increase</button>
+      <ol>
+          <li v-for="post in postList" :key="post">{{post.content}}</li>
+      </ol>
+
       <NewPostForm @submitedForm="addPost" />
     </div>
   </div>
@@ -13,16 +15,12 @@
     import { ref } from "vue";
     import NewPostForm from '../components/NewPostForm'
 
-
-
-    const counter = ref(0);
-
-    function increment() {
-        counter.value += 1;
-    }
+    let postList = ref([])
 
     function addPost(payload){
         console.log('event in parent', payload)
+        postList.value.push(payload)
+        console.log('postList.value', postList.value)
     }
 </script>
 
